b00e9291fbfda171b7d2acb89ba775c93e142ebb
kouheisanpu3-afk <kouheisanpu3@gmail.com>
2026-08-23 13:15:23 +0900

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -1,1 +1,35 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+} = require("discord.js");
 
+module.exports = (client) => {
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isButton()) return;
+
+    // チケット作成ボタン
+    if (interaction.customId === "ticket_create") {
+      const channel = await interaction.guild.channels.create({
+        name: `ticket-${interaction.user.username}`,
+        type: ChannelType.GuildText,
+        permissionOverwrites: [
+          {
+            id: interaction.guild.id,
+            deny: ["ViewChannel"],
+          },
+          {
+            id: interaction.user.id,
+            allow: ["ViewChannel", "SendMessages"],
+          },
+        ],
+      });
+
+      await interaction.reply({
+        content: `チケット作成しました ${channel}`,
+        ephemeral: true,
+      });
+    }
+  });
+};
