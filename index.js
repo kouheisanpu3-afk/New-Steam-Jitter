39ae0e98e6de1559cf29d5edc7cccada11d2ccc9
kouheisanpu3-afk <kouheisanpu3@gmail.com>
2026-08-23 11:09:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ client.once(Events.ClientReady, async () => {
         .setThumbnail(NO_ENTRY_ICON)
         .setDescription(
           "## 認証\n\n" +
-          "### このチャンネルにメッセージを送信しないでください\n\n" +
+          "このチャンネルにメッセージを送信しないでください\n\n" +
           "このチャンネルはスパムボットを検知するために使用されます。\n" +
           "メッセージを送信したユーザーは即座にキックされます。\n\n" +
           `${rulesText}に同意したものとみなされます。`
@@ -99,7 +99,7 @@ client.once(Events.ClientReady, async () => {
         .setThumbnail(NO_ENTRY_ICON)
         .setDescription(
           "## Verification\n\n" +
-          "### DO NOT SEND MESSAGES IN THIS CHANNEL\n\n" +
+          "DO NOT SEND MESSAGES IN THIS CHANNEL\n\n" +
           "This channel is used to detect spam bots.\n" +
           "Users will be kicked immediately.\n\n" +
           `By continuing, you agree to the ${tosText}.`
@@ -133,7 +133,7 @@ client.once(Events.ClientReady, async () => {
         .setColor(0x6C8EA4)
         .setThumbnail(NO_ENTRY_ICON)
         .setDescription(
-          "## このチャンネルにメッセージを送信しないでください\n" +
+          "このチャンネルにメッセージを送信しないでください\n" +
           "このチャンネルはスパムボットを検知するために使用されます。メッセージを送信したユーザーは即座にキックされます。"
         );
 
@@ -141,7 +141,7 @@ client.once(Events.ClientReady, async () => {
         .setColor(0x6C8EA4)
         .setThumbnail(NO_ENTRY_ICON)
         .setDescription(
-          "## DO NOT SEND MESSAGES IN THIS CHANNEL\n" +
+          "DO NOT SEND MESSAGES IN THIS CHANNEL\n" +
           "This channel is used to detect spam bots. Any user who sends a message here will be kicked immediately."
         );
 
