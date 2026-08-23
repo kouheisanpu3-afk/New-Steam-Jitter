fa7cb313622530bd359e62094ecbd2ec31970e40
kouheisanpu3-afk <kouheisanpu3@gmail.com>
2026-08-23 15:52:57 +0900

    Update kick.js

--- kick.js
+++ kick.js
@@ -5,18 +5,18 @@ const KICK_CHANNEL_ID = "1540932243826942002";
 // グレー寄りブルー
 const EMBED_COLOR = 0x5f6f82;
 
-// 🚫画像（指定URL）
-const NO_ENTRY_IMG = "🚫";
+// 🚫画像URL
+const NO_ENTRY_IMG = "https://images-ext-1.discordapp.net/external/V3wsBTSebz_y5_eqHOENkSM6E2SRWyZ0jE66pG9qFKs/https/emojicdn.elk.sh/%F0%9F%9A%AB?format=webp";
 
 // 日本語
 const JP_TEXT =
-`${NO_ENTRY_IMG} このチャンネルにメッセージを送信しないでください
-このチャンネルはスパムボットを検知するために使用されます。メッセージを送信したユーザーは即座にキックされます。`;
+`このチャンネルにメッセージを送信しないでください
+このチャンネルはスパムボットを検知するために使用されます。メッセージを送信したユーザーは即座にキックされます。 ${NO_ENTRY_IMG}`;
 
 // 英語
 const EN_TEXT =
-`${NO_ENTRY_IMG} DO NOT SEND MESSAGES IN THIS CHANNEL
-This channel is used to detect spam bots. Any user who sends a message here will be kicked immediately.`;
+`DO NOT SEND MESSAGES IN THIS CHANNEL
+This channel is used to detect spam bots. Any user who sends a message here will be kicked immediately. ${NO_ENTRY_IMG}`;
 
 module.exports = (client) => {
 
